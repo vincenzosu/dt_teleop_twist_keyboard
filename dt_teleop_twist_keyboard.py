b795ea64faa1ff11321d4d3b233ad45fb5c89a73
Vincenzo Suriani <vincsur@gmail.com>
2020-05-01 19:08:15 +0200

    math

--- dt_teleop_twist_keyboard.py
+++ dt_teleop_twist_keyboard.py
@@ -83,7 +83,7 @@ def vels(speed,turn):
 if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
 #$ rostopic pub -r 100 /default/wheels_driver_node/wheels_cmd duckietown_msgs/WheelCmqStamped '{vel_left:  1.0, vel_right: 1.0}'
-    pub = rospy.Publisher('duckietown_msgs/WheelsCmqStamped', duckietown_msgs/WheelCmdStamped, queue_size = 1)
+    pub = rospy.Publisher('duckietown_msgs/WheelsCmqStamped', duckietown_msgs/WheelsCmdStamped, queue_size = 1)
     rospy.init_node('dt_teleop_twist_keyboard')
 
     speed = rospy.get_param("~speed", 0.5)
